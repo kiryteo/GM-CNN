1ffae40efb28c2f9b3f3e7edc8b3dc5290817d0e
kiryteo <ashwin.samudre@gmail.com>
2024-06-04 18:18:52 -0700

    max and avg pool in gmpool added

--- gm_pooling/gmpool.py
+++ gm_pooling/gmpool.py
@@ -6,25 +6,62 @@ from itertools import product
 from .pool_utils import generate_elements, get_nbr_elements, get_subgroup, get_subgroup_cosets, get_indices
 
 class GMPool(nn.Module):
-    def __init__(self, group, order):
+    """
+    A pooling layer that uses group matrices for cyclic and dihedral groups.
+    """
+
+    def __init__(self, group, order, pool='max'):
+        """
+        Initializes a GMPool module.
+
+        Args:
+            group (str): The type of group. Can be 'cyclic' or 'dihedral'.
+            order (int): The order of the group.
+            pool (str): The pooling method to use. Can be 'max' or 'avg'.
+        """
         super().__init__()
 
-        #self.group = group
-        #self.order = order
+        self.group = group
+        self.order = order
+        self.pool = pool
 
-        elements = generate_elements(group, order)
-        kron_elements = [(a, b) for a, b in product(elements, repeat=2)]
-        # self.kron_prod_strings = [a.__str__() + b.__str__() for a, b in product(self.elements, repeat=2)]
-        kron_prod_strings = [a.__str__() + b.__str__() for a, b in kron_elements]
+        # Generate the group elements
+        group_elements = generate_elements(self.group, self.order)
 
-        nbr_elements = get_nbr_elements(kron_elements, 4)
-        subgroup = get_subgroup(order)
-        cosets = get_subgroup_cosets(subgroup, nbr_elements)
-        self.indices = get_indices(cosets, kron_prod_strings)
+        # Generate direct product of the group elements
+        direct_prod_elements = [(a, b) for a, b in product(group_elements, repeat=2)]
+        direct_prod_strings = [a.__str__() + b.__str__() for a, b in direct_prod_elements]
+
+        # Get the neighborhood elements
+        neighborhood_elements = get_nbr_elements(direct_prod_elements, 4)
+
+        # Get the subgroup
+        subgroup = get_subgroup(self.order)
+
+        # Get the cosets corresponding to the neighborhood elements
+        cosets = get_subgroup_cosets(subgroup, neighborhood_elements)
+
+        # Get the indices of the coset elements in the Kronecker product strings
+        self.indices = get_indices(cosets, direct_prod_strings)
 
     def forward(self, x):
-        #indices = get_indices(self.cosets, self.kron_prod_strings)
-        indices = self.indices
-        op = x[:, :, :, indices]
-        mp = torch.max(op, -2).values
-        return mp
+        """
+        Performs forward pass of the GMPool module.
+
+        Args:
+            x (torch.Tensor): The input tensor.
+
+        Returns:
+            torch.Tensor: The pooled output tensor.
+        """
+        # Extract the relevant indices from the input tensor
+        pooled_input = x[:, :, :, self.indices]
+
+        # Perform the pooling operation
+        if self.pool == 'max':
+            pooled_output = torch.max(pooled_input, dim=-2).values
+        elif self.pool == 'avg':
+            pooled_output = torch.mean(pooled_input, dim=-2)
+        
+        return pooled_output
+
