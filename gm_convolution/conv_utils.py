c8337bd3d62f27d824dbc49f327aefbd13b473b8
kiryteo <ashwin.samudre@gmail.com>
2024-06-04 18:14:55 -0700

    gmconv utils refactor

--- gm_convolution/conv_utils.py
+++ gm_convolution/conv_utils.py
@@ -1,8 +1,18 @@
-
 from group_element.cyclic_group_element import CyclicGroupElement
 from group_element.dihedral_group_element import DihedralElement
 
 def generate_neighborhood(group, n, t):
+    """
+    Generates a neighborhood of group elements.
+
+    Args:
+        group (str): The type of group ('dihedral' or 'cyclic').
+        n (int): The order of the group.
+        t (int): The range of elements to generate around zero.
+
+    Returns:
+        list of str: The string representations of the neighborhood elements.
+    """
     neighborhood = []
 
     if group == 'dihedral':
@@ -12,11 +22,20 @@ def generate_neighborhood(group, n, t):
             neighborhood.append(r_element.__str__())
             neighborhood.append(f_element.__str__())
     elif group == 'cyclic':
-        for i in range(-t, t+1):
+        for i in range(-t, t + 1):
             element = CyclicGroupElement(i, n)
             neighborhood.append(element.__str__())
 
     return neighborhood
 
 def get_nbrhood_elements(nbrhood):
+    """
+    Computes the direct product of neighborhood elements.
+
+    Args:
+        nbrhood (list of str): The neighborhood elements.
+
+    Returns:
+        list of str: The direct product of neighborhood elements.
+    """
     return [e1 + e2 for e1 in nbrhood for e2 in nbrhood]
